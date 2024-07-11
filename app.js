e6ee2a792c9bccd37a4d16095f8cce76eaa19f4c
kyledinardi <94251418+kyledinardi@users.noreply.github.com>
2024-07-11 13:32:07 -0400

    Create express app

---
+++ app.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const compression = require('compression');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const express = require('express');
+const logger = require('morgan');
+const debug = require('debug')('odin-messaging-app-backend:app');
+const helmet = require('helmet');
+const path = require('path');
+const RateLimit = require('express-rate-limit');
+const indexRouter = require('./routes/index');
+
+const app = express();
+const mongodb = process.env.MONGODB;
+
+async function main() {
+  await mongoose.connect(mongodb);
+}
+
+main().catch((err) => debug(err));
+
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 100,
+});
+
+app.use(limiter);
+app.use(cors());
+app.use(helmet());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(compression());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+module.exports = app;
