a3efdc5336958bb7f62c4af6b354df21bfe0a316
kyledinardi <94251418+kyledinardi@users.noreply.github.com>
2024-07-16 13:47:59 -0400

    Improve passport local strategy

--- helper/passport.js
+++ helper/passport.js
@@ -9,13 +9,13 @@ passport.use(
   new LocalStragetgy(async (username, password, done) => {
     try {
       const user = await User.findOne({ username });
-      const match = await bcrypt.compare(password, user.password);
+      let match = false;
 
-      if (!user) {
-        return done(null, false, { message: 'Incorrect username' });
+      if (user) {
+        match = await bcrypt.compare(password, user.password);
       }
       if (!match) {
-        return done(null, false, { message: 'Incorrect password' });
+        return done(null, false, { message: 'Incorrect username or password' });
       }
 
       return done(null, user);
